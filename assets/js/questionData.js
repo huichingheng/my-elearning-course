59e025d730623a3d1bebfd668f9bf436aa4aea71
huichingheng <huichingheng@gmail.com>
2020-06-03 16:37:24 +0800

    added drag and drop quiz

--- assets/js/questionData.js
+++ assets/js/questionData.js
@@ -1,12 +1,19 @@
 const questionData = [
   {
     id: "1",
-    question_stem: "What is the colour of the sky?",
-    answer: "c"
+    question_stem: "COVID-19 is a",
+    answer: "A",
   },
   {
     id: "2",
-    question_stem: "How many hours a day?",
-    answer: "d"
-  }
+    question_stem:
+      "What should I do if I do not have water and soap with me to clean my hands?",
+    answer: "B",
+  },
+  {
+    id: "3",
+    question_stem:
+      "Protect people around me, I should sneeze and cough _______ or in a _______ and throw it immediately in a bin with a cover, then _______.",
+    answer: "in the inside of my elbow, paper napkin, wash my hands",
+  },
 ];
